9d5ce4c95a951679ed4c6c9946223a6b8146a4c3
stat1c <fynn.weyrich@web.de>
2021-06-08 17:06:15 +0200

    first pipeline test

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import * as bodyParser from "body-parser";
+import compression from "compression";
+import cors from "cors";
+import morgan from 'morgan';
+
+//setup environment variables
+import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
+
+dotenv.config();
+
+//setup app
+const app = express();
+
+//setup app config
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(compression());
+app.use(cors());
+app.use(morgan('dev'));
+app.use('/', (req: express.Request, res: express.Response) => {  
+    res.send("Hello World");
+});
+
+//listen on port 3001
+app.listen(process.env.PORT, () => console.log("Server started on Port: " + process.env.PORT));
