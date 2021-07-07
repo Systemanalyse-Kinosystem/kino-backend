039755b67b941471e31560fc2b9ded9d82d81912
lumaghg <rotkehlchen567@gmail.com>
2021-07-07 16:31:09 +0200

    removed commented code

--- src/api/middlewares/auth.middlewares.ts
+++ src/api/middlewares/auth.middlewares.ts
@@ -25,64 +25,6 @@ class authenticationMiddleware {
             }
         }
     }
-    static async authUser(req: any, res: Response, next: NextFunction) {
-        const secret = <string>process.env.JWT_SECRET;
-        try {
-            const verificationResponse = <IDataStoredInToken>jwt.verify(req.headers.auth, secret);
-            const user = await User.findOne({
-                id: verificationResponse._id,
-                role: { '$in': ['user', 'admin'] }
-            });
-            if (user) {
-                req.user = user;
-                next();
-            } else {
-                res.status(401).json({ err: "An Error ocurred" });
-            }
-        } catch (error) {
-            res.status(401).json(error);
-        }
-    }
-/*
-    static async authCustomer(req: any, res: Response, next: NextFunction) {
-        const secret = <string>process.env.JWT_SECRET;
-        try {
-            const verificationResponse = <IDataStoredInToken>jwt.verify(req.headers.auth, secret);
-            const user = await User.findOne({
-                id: verificationResponse._id,
-            });
-            if (user) {
-                req.user = user;
-                next();
-            } else {
-                res.status(401).json({ err: "An Error ocurred" });
-            }
-        } catch (error) {
-            res.status(401).json(error);
-        }
-
-    }
-
-    static async authAdmin(req: any, res: Response, next: NextFunction) {
-        const secret = <string>process.env.JWT_SECRET;
-        try {
-            const verificationResponse = <IDataStoredInToken>jwt.verify(req.headers.auth, secret);
-            const user = await User.findOne({
-                id: verificationResponse._id,
-                role: 'admin'
-            });
-            if (user) {
-                req.user = user;
-                next();
-            } else {
-                res.status(401).json({ err: "An Error ocurred" });
-            }
-        } catch (error) {
-            res.status(401).json(error);
-        }
-
-    }
-    */
 }
 
 
