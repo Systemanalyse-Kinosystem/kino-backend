e8e16db2d1c1bc06ab56b06f8f3e8abf9f4ef5f3
lumaghg <rotkehlchen567@gmail.com>
2021-09-06 16:16:03 +0200

    added mocha to dependencies

--- src/test/demo.test.ts
+++ src/test/demo.test.ts
@@ -1,14 +1,3 @@
 import request from "supertest";
 import app from "./../app";
 
-describe('GET /', function() {
-    it('responds without errors', function(done) {
-      request(app)
-        .get('/')
-        .expect(200)
-        .end((err:Error, res:request.Response):void => {
-            if(err) return done(err);
-            return done();
-        })
-    });
-  });
