6919e3c49acb90ad2f29f842d56837d3956d85d3
lumaghg <rotkehlchen567@gmail.com>
2021-09-08 09:14:47 +0200

    removed multi-cinema support

--- src/api/routes/routes.ts
+++ src/api/routes/routes.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import authenticationRouter from "../routes/authentication/authentication.router";
 import userRouter from "./user/user.router";
-import cinemaRouter from "./z_old_cinema/cinema.router";
+//import cinemaRouter from "./z_old_cinema/cinema.router";
 import hallRouter from "./hall/hall.router";
 let router = express.Router();
 
 // define test route
 router.use('/user', userRouter);
-router.use('/cinema', cinemaRouter);
+//router.use('/cinema', cinemaRouter);
 router.use('/hall', hallRouter);
 router.use('/', authenticationRouter);
 
