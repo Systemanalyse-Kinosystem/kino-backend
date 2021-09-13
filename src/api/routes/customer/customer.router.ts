5dc07a039ff487ff0eaba5c52f675a5236711ee0
lumaghg <rotkehlchen567@gmail.com>
2021-09-13 15:58:52 +0200

    added protection back for /me routes

--- src/api/routes/customer/customer.router.ts
+++ src/api/routes/customer/customer.router.ts
@@ -4,12 +4,12 @@ import authenticationMiddleware from "./../../middlewares/auth.middlewares"
 
 let router = express.Router();
 
-router.get('/me', /*authenticationMiddleware.getAuthenticationMiddleware(['customer']),*/ customerController.getLoggedInCustomer);
+router.get('/me', authenticationMiddleware.getAuthenticationMiddleware(['customer']), customerController.getLoggedInCustomer);
 router.get('/', /*authenticationMiddleware.getAuthenticationMiddleware(['admin', 'user']),*/ customerController.getCustomerList);
 router.get('/:id', /*authenticationMiddleware.getAuthenticationMiddleware(['admin']),*/ customerController.getCustomerById);
 //router.post('/', /*authenticationMiddleware.getAuthenticationMiddleware(['user', 'admin']),*/ customerController.createCustomer); 
 //see /register in authentication module
-router.put('/me', /*authenticationMiddleware.getAuthenticationMiddleware(['customer']),*/ customerController.updateLoggedInCustomer);
+router.put('/me', authenticationMiddleware.getAuthenticationMiddleware(['customer']), customerController.updateLoggedInCustomer);
 router.put('/:id', /*authenticationMiddleware.getAuthenticationMiddleware(['admin']),*/ customerController.updateCustomerById);
 router.delete('/:id', /*authenticationMiddleware.getAuthenticationMiddleware(['customer', 'admin']),*/ customerController.deleteCustomerById);
 router.delete('/', /*authenticationMiddleware.getAuthenticationMiddleware(['admin']),*/ customerController.deleteCustomers);
