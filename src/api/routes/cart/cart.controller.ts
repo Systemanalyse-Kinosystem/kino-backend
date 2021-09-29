43e7be655168530d38be3f4880e6885dc20e5ac3
lumaghg <rotkehlchen567@gmail.com>
2021-09-29 10:46:24 +0200

    started refactoring from callbacks to promises

--- src/api/routes/cart/cart.controller.ts
+++ src/api/routes/cart/cart.controller.ts
@@ -11,80 +11,64 @@ import utils from "../../utils/utils";
 
 export default class cartController {
 
-    static getCartById(req: Request, res: Response) {
-        Cart.findOne({ _id: req.params.id }, (err: CallbackError | null, cart: ICart | null) => {
-            if (!cart || err) { return res.status(500).json({ err: err ? err : "Not found" }); }
+    static async getCartById(req: Request, res: Response) {
+        try {
+            let cart = await Cart.findOne({ _id: req.params.id });
+            if (!cart) { return res.status(400).json({ err: "Not found" }); }
             res.json(cart);
-        });
+        } catch (e) { res.status(500).json(e); }
     };
 
-    static createCart(req: Request, res: Response) {
-        Cart.create({ tickets: [] }, (err: CallbackError | null, cart: ICartNotPopulated | null) => {
-            if (err || !cart) { return res.status(400).json({ err: err ? err : "Not found" }); }
+    static async createCart(req: Request, res: Response) {
+        try {
+            let cart = await Cart.create({ tickets: [] });
+            if (!cart) { return res.status(400).json({ err: "Not found" }); }
             res.status(201).json(cart);
-        });
+        } catch (e) { res.status(500).json(e); }
     }
 
-    static checkOutCartBookWithCartId(req: Request, res: Response) {
-        Cart.findById(req.params.id, (err: CallbackError | null, cart: ICartNotPopulated | null) => {
-            if (err) { return res.status(500).json({ err: err }); }
+    static async checkOutCartBookWithCartId(req: Request, res: Response) {
+        try {
+            let cart = await Cart.findById(req.params.id);
             if (!cart) { return res.status(400).json({ err: "Cart not found" }); }
 
-            Ticket.updateMany({ _id: { $in: cart.tickets } }, { "$set": { status: "valid", user: req.body } }, { "multi": true }, (err: CallbackError | null, writeResult: any) => {
-                if (err) { return res.status(500).json({ err: err }); }
+            await Ticket.updateMany({ _id: { $in: cart.tickets } }, { "$set": { status: "valid", user: req.body } }, { "multi": true });
 
-                Cart.findOneAndUpdate({ _id: req.params.id }, { tickets: [], user: req.body }, { new: true }, (err: CallbackError | null, cartUpdated: ICartNotPopulated | null) => {
-                    if (err) { return res.status(500).json({ err: err }); }
-                    if (!cartUpdated) { return res.status(400).json({ err: "Not found" }); }
-                    res.json(cartUpdated);
-                    //send Mail
-                    let mailText = "Sie haben folgende Tickets bestellt: "
-                    for (let ticket of cart.tickets) {
-                        mailText += `TicketID: ${ticket} `
-                    }
-                    utils.getNodeMailerTransporter().sendMail({
-                        from: 'noreply.kinosystem@gmail.com',
-                        to: req.body.email,
-                        subject: 'Ihre Bestellung',
-                        text: mailText
-                    }, (err, info) => {
-                        if (err) { console.error(err); }
-                    });
+            let cartUpdated = await Cart.findOneAndUpdate({ _id: req.params.id }, { tickets: [] }, { new: true });
+            if (!cartUpdated) { return res.status(400).json({ err: "Not found" }); }
+            res.json(cartUpdated);
 
-                });
+            //send Mail
+            let mailText = "Sie haben folgende Tickets bestellt: "
+            for (let ticket of cart.tickets) {
+                mailText += `TicketID: ${ticket} `
+            }
+
+            await utils.getNodeMailerTransporter().sendMail({
+                from: 'noreply.kinosystem@gmail.com',
+                to: req.body.email,
+                subject: 'Ihre Bestellung',
+                text: mailText
             });
-        });
+        } catch (e) { res.status(500).json(e); }
     }
+
+
     /* NOT NEEDED FOR MVP
         static checkOutCartBookWithLogin(req: Request, res: Response) {
             //implement
         }
     */
 
-    static checkOutCartReserveWithLogin(req: Request, res: Response) {
-        Cart.findById(req.params.id, (err: CallbackError | null, cart: ICartNotPopulated | null) => {
-            if (err) { return res.status(500).json({ err: err }); }
-            if (!cart) { return res.status(400).json({ err: "Cart not found" }); }
+    static async checkOutCartReserveWithLogin(req: Request, res: Response) {
+        let cart = await Cart.findById(req.params.id);
+        if (!cart) { return res.status(400).json({ err: "Cart not found" }); }
 
-            Ticket.updateMany({ _id: { $in: cart.tickets } }, { "$set": { status: "reserved", userID: (<IRequestWithUser>req).user.id } }, { "multi": true }, (err: CallbackError | null, writeResult: any) => {
-                if (err) { return res.status(500).json({ err: err }); }
+        await Ticket.updateMany({ _id: { $in: cart.tickets } }, { "$set": { status: "reserved", userID: (<IRequestWithUser>req).user.id } }, { "multi": true });
 
-                Cart.findOneAndUpdate({ _id: req.params.id }, { tickets: [] }, { new: true }, (err: CallbackError | null, cart: ICartNotPopulated | null) => {
-                    if (err) { return res.status(500).json({ err: err }); }
-                    if (!cart) { return res.status(400).json({ err: "Not found" }); }
-                    //sendMail
-                    res.json(cart);
-                });
-            });
-        });
+        let updatedCart = await Cart.findOneAndUpdate({ _id: req.params.id }, { tickets: [] }, { new: true });
+        if (!updatedCart) { return res.status(400).json({ err: "Not found" }); }
+        //sendMail
+        res.json(updatedCart);
     }
-
 }
-
-/*
-booking:
-findCart
-mark the tickets as booked
-clear cart
-send mail
-*/
