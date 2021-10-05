51fa563c3a668f84b3847eb68221546aa9039ed7
lumaghg <rotkehlchen567@gmail.com>
2021-10-05 17:08:46 +0200

    refactored screening controller

--- src/api/routes/screening/screening.controller.ts
+++ src/api/routes/screening/screening.controller.ts
@@ -1,6 +1,6 @@
 //replace screening, Screening, IScreening
 import { Request, Response } from 'express';
-import { CallbackError} from 'mongoose';
+import { CallbackError } from 'mongoose';
 import Screening from '../../models/screening.model';
 import IScreening from '../../interfaces/screening.interface';
 import Hall from '../../models/hall.model';
@@ -10,47 +10,52 @@ import Ticket from '../../models/ticket.model';
 export default class screeningController {
 
     static async getScreeningList(req: Request, res: Response) {
-        //build queryOptions
-        let sortOptions: any = {};
-        sortOptions[<string>req.query.orderby] = <string>req.query.orderdir;
-        let perPage = req.query.perPage ? parseInt(<string>req.query.perPage) : 10;
-        let dateOptions = req.query.dateBegin && req.query.dateEnd ? {startDate: {$gte : new Date(<string>req.query.dateBegin), $lt : new Date(<string>req.query.dateEnd)}}: {};
-        let movieOptions = req.query.movie ? {_id: <string>req.query.movie}: {};
+        try {
+            //build queryOptions
+            let sortOptions: any = {};
+            sortOptions[<string>req.query.orderby] = <string>req.query.orderdir;
+            let perPage = req.query.perPage ? parseInt(<string>req.query.perPage) : 10;
+            let dateFilter = req.query.dateBegin && req.query.dateEnd ? { startDate: { $gte: new Date(<string>req.query.dateBegin), $lt: new Date(<string>req.query.dateEnd) } } : {};
+            let movieFilter = req.query.movie ? { movie: <string>req.query.movie } : {};
+            let queryOptions = {
+                skip: parseInt(<string>req.query.page) * perPage,
+                limit: perPage,
+                sort: sortOptions,
+                populate: ['movie', 'hall']
+            };
 
-        Screening.find({...movieOptions, ...dateOptions}, null, {
-            skip: parseInt(<string>req.query.page) * perPage,
-            limit: perPage,
-            sort: sortOptions,
-            populate: ['movie', 'hall']
-        }, (err: CallbackError | null, screenings: IScreening[] | null) => {
-            if (err) { return res.status(500).json(err) }
-            if (!screenings) { return res.status(500).json({ err: err ? err : "Not found" }); }
+            let screenings = await Screening.find({ ...movieFilter, ...dateFilter }, null, queryOptions);
+            if (!screenings) { return res.status(500).json({ err: "Not found" }); }
             res.json(screenings);
-        });
+        } catch (e) { res.status(500).json({ err: 'An Error occured' }); }
     };
 
-    static getScreeningById(req: Request, res: Response) {
-        Screening.findOne({ _id: req.params.id }, null, { populate: ['movie', 'hall'] }, (err: CallbackError | null, screening: IScreening | null) => {
-            if (!screening || err) { return res.status(500).json({ err: err ? err : "Not found" }); }
+    static async getScreeningById(req: Request, res: Response) {
+        try {
+            let screening = await Screening.findOne({ _id: req.params.id }, null, { populate: ['movie', 'hall'] });
+            if (!screening) { return res.status(500).json({ err: "Not found" }); }
             res.json(screening);
-        });
+        } catch (e) { res.status(500).json({ err: 'An Error occured' }); }
+
     };
 
-    static getScreeningByMovieId(req: Request, res: Response) {
+    static async getScreeningByMovieId(req: Request, res: Response) {
         //build sortOptions and seachOptions
-        let sortOptions: any = {};
-        sortOptions[<string>req.query.orderby] = <string>req.query.orderdir;
-        let perPage = req.query.perPage ? parseInt(<string>req.query.perPage) : 10;
-        Screening.find({ movie: req.params.id }, null, {
-            skip: parseInt(<string>req.query.page) * perPage,
-            limit: perPage,
-            sort: sortOptions,
-            populate: ['movie', 'hall']
-        }, (err: CallbackError | null, screenings: IScreening[] | null) => {
-            if (err) { return res.status(500).json(err) }
-            if (!screenings) { return res.status(500).json({ err: err ? err : "Not found" }); }
+        try {
+            let sortOptions: any = {};
+            sortOptions[<string>req.query.orderby] = <string>req.query.orderdir;
+            let perPage = req.query.perPage ? parseInt(<string>req.query.perPage) : 10;
+            let queryOptions = {
+                skip: parseInt(<string>req.query.page) * perPage,
+                limit: perPage,
+                sort: sortOptions,
+                populate: ['movie', 'hall']
+            };
+            let screenings = await Screening.find({ movie: req.params.id }, null, queryOptions);
+
+            if (!screenings) { return res.status(500).json({ err: "Not found" }); }
             res.json(screenings);
-        });
+        } catch (e) { res.status(500).json({ err: 'An Error occured' }); }
     };
 
     /* DEACTIVATED FOR MVP
